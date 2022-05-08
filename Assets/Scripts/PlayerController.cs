a7953973ff9869aa06eca0aaccc83bf3de62dda1
DragonWF <plarisanmarc@gmail.com>
2022-05-08 15:33:55 +0800

    Implemented smooth movement with acceleration and friction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 10f;
-    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] const float baseSpeed = 5f;
+    [SerializeField] const float baseSteerSpeed = 300f;
+
+    const float maxAcceleration = 15f;
+    float acceleration = 0f;
+    bool reversing;
 
     void Start()
     {
@@ -14,15 +18,40 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        CarMovement();
+        CarSteering();
+    }
+
+    void CarMovement()
+    {
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
         {
-            var speed = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            reversing = Input.GetKey(KeyCode.DownArrow);
+
+            acceleration += 0.15f;
+            if (acceleration >= maxAcceleration)
+                acceleration = maxAcceleration;
+
+            var speed = Input.GetAxis("Vertical") * Time.deltaTime * (baseSpeed + acceleration);
             transform.Translate(0, speed, 0);
         }
+        else
+        {
+            acceleration -= 0.2f;
+            if (acceleration <= 0)
+                acceleration = 0;
 
+            var force = reversing ? -acceleration : acceleration;
+            var friction = Time.deltaTime * force;
+            transform.Translate(0, friction, 0);
+        }
+    }
+
+    void CarSteering()
+    {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {
-            var rotateSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * steerSpeed;
+            var rotateSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * baseSteerSpeed;
             transform.Rotate(0, 0, rotateSpeed);
         }
     }
