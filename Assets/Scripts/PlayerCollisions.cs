03716cb4ffb93f14680e2aa6edcc02ef1a7c7363
DragonWF <plarisanmarc@gmail.com>
2022-05-08 20:39:17 +0800

    Added base class

---
+++ Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+    }
+}
