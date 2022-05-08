5a80889acffd175cd15e7167008a2b69c2e0534c
DragonWF <plarisanmarc@gmail.com>
2022-05-08 20:48:46 +0800

    Added main functionality for collision events

--- Assets/Scripts/PlayerCollisions.cs
+++ Assets/Scripts/PlayerCollisions.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
+    PlayerController player;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        
+        CollisionEvent(other.tag, true);
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
+        CollisionEvent(other.tag, false);
+    }
 
+    void CollisionEvent(string tag, bool isEntering)
+    {
+        switch (tag)
+        {
+            case "Road":
+                player.ChangeRoadStatus(tag, isEntering);
+                break;
+            case "Intersection":
+                player.ChangeRoadStatus(tag, isEntering);
+                break;
+        }
     }
 }
