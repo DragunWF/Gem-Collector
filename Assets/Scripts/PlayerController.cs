b3c197cd9b3cc04633e4a0febd75d2cbff2ef7fb
DragonWF <plarisanmarc@gmail.com>
2022-05-08 15:10:06 +0800

    Finished basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float steerSpeed = 300f;
+
     void Start()
     {
 
@@ -11,6 +14,16 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            var speed = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+            transform.Translate(0, speed, 0);
+        }
 
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            var rotateSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * steerSpeed;
+            transform.Rotate(0, 0, rotateSpeed);
+        }
     }
 }
