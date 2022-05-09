3b579d18e58fbdc1d40fcd4e616ae7bae8fde5e8
DragonWF <plarisanmarc@gmail.com>
2022-05-09 22:16:22 +0800

    Added sound effect on death

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerState : MonoBehaviour
 {
     ParticleSystem explosionParticle;
+    AudioSource explosionSoundEffect;
     Timer timer;
 
     public bool IsAlive { get; private set; }
 
     void Start()
     {
+        explosionSoundEffect = GetComponent<AudioSource>();
         explosionParticle = GetComponent<ParticleSystem>();
         timer = FindObjectOfType<Timer>();
         IsAlive = true;
@@ -18,10 +20,11 @@ public class PlayerState : MonoBehaviour
 
     void Update()
     {
-        if (IsAlive && timer.TimeValue <= 0)
+        if (timer.TimeValue <= 0 && IsAlive)
         {
             IsAlive = false;
             explosionParticle.Play();
+            explosionSoundEffect.Play();
         }
     }
 }
