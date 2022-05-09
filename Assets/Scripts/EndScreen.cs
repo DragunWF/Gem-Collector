05b26d7447ced7bb8f0018301bcd23576e4006fe
DragonWF <plarisanmarc@gmail.com>
2022-05-10 00:54:41 +0800

    Implemented main functionality for end screen

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EndScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] TextMeshProUGUI endText;
+    [SerializeField] Button retryButton;
 
+    ScoreKeeper scoreKeeper;
+    Image background;
+
+    PlayerState playerState;
+    bool gameEnding;
+    bool isDisplayed;
+    float alphaSlider = 0;
+
+    public void TriggerEndScreen()
+    {
+        gameEnding = true;
     }
 
-    // Update is called once per frame
+    void Awake()
+    {
+        playerState = FindObjectOfType<PlayerState>();
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        background = GameObject.Find("Background").GetComponent<Image>();
+    }
+
+    void Start()
+    {
+        background.color = new Color(0, 0, 0, 0);
+        retryButton.gameObject.SetActive(false);
+        endText.gameObject.SetActive(false);
+    }
+
     void Update()
     {
+        if (gameEnding && alphaSlider < 255)
+        {
+            alphaSlider += Time.deltaTime * 85;
+            background.color = new Color32(0, 0, 0, (byte)alphaSlider);
+        }
 
+        if (!isDisplayed && alphaSlider >= 255)
+        {
+            isDisplayed = true;
+            Invoke("DisplayUI", 1);
+        }
+    }
+
+    void DisplayUI()
+    {
+        retryButton.gameObject.SetActive(true);
+        endText.gameObject.SetActive(true);
+        endText.text = "Congrats! You got a score of\n" + scoreKeeper.Score;
     }
 }
