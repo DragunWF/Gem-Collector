ae4a1c8efcda99d9152a6b6d278d29913d1da6a3
DragonWF <plarisanmarc@gmail.com>
2022-05-09 19:44:48 +0800

    Implemented main functionality

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
+    ParticleSystem explosionParticle;
+    Timer timer;
+
+    public bool IsAlive { get; private set; }
+
     void Start()
     {
-
+        explosionParticle = GetComponent<ParticleSystem>();
+        timer = FindObjectOfType<Timer>();
+        IsAlive = true;
     }
 
     void Update()
     {
-
+        if (IsAlive && timer.TimeValue <= 0)
+        {
+            IsAlive = false;
+            explosionParticle.Play();
+        }
     }
 }
