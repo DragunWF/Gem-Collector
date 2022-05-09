2de9952b6d32da6d73ea768e1540bbac28f01156
DragonWF <plarisanmarc@gmail.com>
2022-05-09 18:35:12 +0800

    Implemented main timer functionality

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,16 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI timerText;
+
+    const float timeLimitValue = 60f;
+    public float TimeValue { get; private set; }
+    bool timerStarted;
+
     void Start()
     {
-        
+        TimeValue = timeLimitValue;
+        Invoke("StartTimer", 3f);
     }
 
     void Update()
     {
-        
+        if (timerStarted)
+        {
+            TimeValue -= Time.deltaTime;
+            if (TimeValue <= 0)
+                TimeValue = 0;
+
+            var seconds = Mathf.RoundToInt(TimeValue);
+            timerText.text = "Time Left: " + seconds;
+        }
+    }
+
+    void StartTimer()
+    {
+        timerStarted = true;
     }
 }
