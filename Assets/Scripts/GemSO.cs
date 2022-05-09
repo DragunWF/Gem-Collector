67c3a0a2c67166833baa011407fe3e23eee11842
DragonWF <plarisanmarc@gmail.com>
2022-05-09 13:14:29 +0800

    Implemented main functionality

--- Assets/Scripts/GemSO.cs
+++ Assets/Scripts/GemSO.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Gem Type", fileName = "New Gem Type")]
 public class GemSO : ScriptableObject
 {
-    void Start()
+    [SerializeField] Sprite[] gemSprites = new Sprite[7];
+    [SerializeField] Color particleColor;
+
+    public Sprite PickRandomShape()
     {
-
-    }
-
-    void Update()
-    {
-
+        return gemSprites[Random.Range(0, gemSprites.Length - 1)];
     }
 }
