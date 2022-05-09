c5c3a87f09cf657d3f6c7d1961f9bbabfc5b1c8f
DragonWF <plarisanmarc@gmail.com>
2022-05-09 13:04:37 +0800

    Constructed base class

---
+++ Assets/Scripts/GemSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSO : ScriptableObject
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
